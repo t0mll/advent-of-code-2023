dcb0673e17a7e8dacd4f95b6bc903c41c9af9c18
Thomas Carlier <carlier.thomas@gmail.com>
2023-12-07 11:57:59 +1000

    day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,17 +2,12 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-fn main() {
-    let mut n = 0;
-    if let Ok(lines) = read_lines("input.txt"){
-        for line in lines {
-            if let Ok(s) = line {
-                n += get_calibration_value(&s)   
-            }
-        }
-    }
+fn part1(file_path: &str){
+    println!("Day 1 Part 1: {}", get_calibration_value(file_path, false));
+}
 
-    println!("{}", n);
+fn part2(file_path: &str) {
+    println!("Day 1 Part 2: {}", get_calibration_value(file_path, true));
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -21,18 +16,43 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn get_calibration_value(s: &str) -> u32 {
-    let mut left = u32::MIN;
-    let mut right = u32::MIN;
+fn get_calibration_value(file_path: &str, replace: bool) -> u32 {
+    let mut result = 0;
 
-     for ch in s.chars() {
-        if ch.is_digit(10) {
-            if left == u32::MIN {
-                    left = ch.to_digit(10).unwrap()
-                }
-            right = ch.to_digit(10).unwrap()
+    if let Ok(lines) = read_lines(file_path.to_string()) {
+        for line in lines {
+            if let Ok(s) = line {
+            let formatted = get_formatted_line(&s, replace);
+            let digits = formatted.chars().filter_map(|c| c.to_digit(10)).collect::<Vec<u32>>();
+
+            result += digits.first().unwrap() * 10 + digits.last().unwrap()
+            }
         }
     }
 
-    right + (left * 10)
+    return result;
+}
+
+fn get_formatted_line(line: &str, replace: bool) -> String {
+    if replace {
+        line.to_string()
+            .replace("one", "one1one")
+            .replace("two", "two2two")
+            .replace("three", "three3three")
+            .replace("four", "four4four")
+            .replace("five", "five5five")
+            .replace("six", "six6six")
+            .replace("seven", "seven7seven")
+            .replace("eight", "eight8eight")
+            .replace("nine", "nine9nine")
+    }
+    else {
+        line.to_string()
+    }
+}
+
+fn main() {
+    let input = "input.txt";
+    part1(input);
+    part2(input);
 }
